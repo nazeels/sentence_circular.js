7ec4b3fa49c70cc9e9a5d78b27833d754d07a6df
nazeels <nazeel.ak@gmail.com>
2013-07-13 10:36:16 +0400

    code structuring

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ var KEY_CODE_UP = 38, KEY_CODE_DOWN = 40;
 var placeHolder = $('#slide_contents');
 //slider
 var index = 0,index_max=lstSentences.length;
+//get height of content to scroll
+var slide_content_height = $($('#slide_contents').children()[0]).height();
 $(document).ready(function () {
   createSliderChildren(lstSentences);
 });
 createSliderChildren = function(sentances){
   sentances.forEach(function(itm){
-    //add as div
     var div=$('<div/>');
     div.append(itm);
     placeHolder.append(div);
@@ -23,28 +24,23 @@ createSliderChildren = function(sentances){
   attachSlider();
 };
 
-
-
 attachSlider = function(){
-//get height of content to scroll
-var slide_content_height = $($('#slide_contents').children()[0]).height();
-
-$('body').on('keyup', function(e){
-    if(e.keyCode === KEY_CODE_DOWN)
-    {
-      if(index < index_max -1){
-          $("#slide_contents").css("transform","translateY("+ ++index * -slide_content_height+"px)");
-    }
-  }
-});
-
-$('body').on('keyup', function(e){
-   if(e.keyCode === KEY_CODE_UP)
-    {
-      if(index > 0){
-          --index;
-          $("#slide_contents").css("transform","translateY("+  index * -slide_content_height +"px)");
+  $('body').on('keyup', function(e){
+      if(e.keyCode === KEY_CODE_DOWN)
+      {
+        if(index < index_max -1){
+            $("#slide_contents").css("transform","translateY("+ ++index * -slide_content_height+"px)");
       }
-  }
-});
-};
+    }
+  });
+
+  $('body').on('keyup', function(e){
+     if(e.keyCode === KEY_CODE_UP)
+      {
+        if(index > 0){
+            --index;
+            $("#slide_contents").css("transform","translateY("+  index * -slide_content_height +"px)");
+        }
+      }
+    });
+  };
