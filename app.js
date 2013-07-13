853faa6c3d2b50011a8c7843c3701c3f7f9bdc53
nazeels <nazeel.ak@gmail.com>
2013-07-13 10:27:59 +0400

    up-down keys are binded to events

--- app.js
+++ app.js
@@ -12,28 +12,6 @@ var placeHolder = $('#slide_contents');
 var index = 0,index_max=lstSentences.length;
 $(document).ready(function () {
   createSliderChildren(lstSentences);
-// 	//indexer to manage rotation of sentances
-// 	var index = 0;
-// 		//get the count of items in sentences
-// var countOfSentences = lstSentences.length;
-// //render the contents inside the slider
-// 	$('body').on('keyup', function(e) {
-//    		//connecting to keyboard
-//    		if(e.keyCode === KEY_CODE_UP)
-//    		{
-//    			//console.log('rotating up');
-//    			if(index < countOfSentences - 1)
-//    				++index
-//    			else if(index === countOfSentences -1)
-//    				index=0;
-   			
-//    			createFormatedText(lstSentences[index]);
-//    		}
-//    		else if(e.keyCode === KEY_CODE_DOWN)
-//    		{
-//    			console.log('rotating down');
-//    		}
-
 });
 createSliderChildren = function(sentances){
   sentances.forEach(function(itm){
@@ -51,16 +29,22 @@ attachSlider = function(){
 //get height of content to scroll
 var slide_content_height = $($('#slide_contents').children()[0]).height();
 
-$('#slide_up').on('click', function(){
-    if(index < index_max -1){
-        $("#slide_contents").css("transform","translateY("+ ++index * -slide_content_height+"px)");
+$('body').on('keyup', function(e){
+    if(e.keyCode === KEY_CODE_DOWN)
+    {
+      if(index < index_max -1){
+          $("#slide_contents").css("transform","translateY("+ ++index * -slide_content_height+"px)");
     }
+  }
 });
 
-$('#slide_down').on('click', function(){
-    if(index > 0){
-        --index;
-        $("#slide_contents").css("transform","translateY("+  index * -slide_content_height +"px)");
-    }
+$('body').on('keyup', function(e){
+   if(e.keyCode === KEY_CODE_UP)
+    {
+      if(index > 0){
+          --index;
+          $("#slide_contents").css("transform","translateY("+  index * -slide_content_height +"px)");
+      }
+  }
 });
 };
